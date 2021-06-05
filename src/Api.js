dbc13002a9270b8874f1f26c83fb6b3ec3b4409f
Maria Terenteva <rtillkriyt@gmail.com>
2021-06-05 12:05:29 +0300

    api, copy button creation

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,32 @@ export default class Api{
     constructor(options) {
         this.options = options;
     }
-    
+
+    shortLink(link){
+        this.link = link;
+        return fetch('https://api-ssl.bitly.com/v4/shorten', {
+            method: 'POST',
+            headers: {
+                authorization: 'aa0400aad11d6a934d5063bbb1ddb504f3994352',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({
+                long_url: `${this.link}`,
+            }),
+        })
+        .then((res) => {
+            if (res.ok) {
+              return res.json();
+            }
+        
+                return Promise.reject(` ${res.status}`);
+          })
+          .then((data) => {
+  
+            return data;
+          })                  
+
+    }
 
 
 
--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import './Main.css';
+import Api from './Api.js';
 
 
 function Main() {
 
+    const [longLink, setLongLink] = React.useState('');
+    const [value, setValue] = React.useState('');
+    const [error, setError] = React.useState('');
+
+    const linkRef = React.useRef();
+
+
+    const handleChange = (e) => {        
+        setValue(e.target.value);
+        setError(handleError(e));
+        setLongLink(e.target.value)
+
+    }
+
+    const api = new Api();
+
+    const handleError = (e) => {
+    
+        if (e.target.value.length === 0) {
+            return 'This is a required field';
+        }
+        else if (!/^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/.test(e.target.value)) {
+            return 'There should be a link here';
+        } else {
+            return '';
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(linkRef.current.value && !error){
+            api.shortLink(linkRef.current.value).then((res) => {
+                setValue(res.link);
+                handleClick(res.link)
+            }).catch((err) => {
+                setError("Error " + err);
+              })
+        }
+        
+    }
+
+    const handleClick = () => {
+        document.querySelector(".main-form__input").select();
+        document.execCommand("copy");
+    }
+
         return (
             <div className="main root__section">
-                <form className="form main-form">
+                <form className="form main-form" onSubmit={handleSubmit}>
                     <label className="main-form__label">Write your link here</label>
-                    <input type="url" className="main-form__input"></input>
-                    <button className="main-form__button">Shorten</button>
-                </form>
+                    <input value={value} ref={linkRef} minLength="5" onChange={handleChange} required type="url" className="main-form__input"></input>
+                    <span  id="error" className="error main-form__error">{error}</span>      
+                                        <button onClick={handleClick} id="copy" className=" main-form__copy">Copy</button>      
+                    <button type="submit"  className="main-form__button">Shorten</button>
+                </form >
             </div>
         );
     
